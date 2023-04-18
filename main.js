b918e0cedbc7126bdbd32764810529385469a204
kengo1226 <kengo1226size@gmail.com>
2023-04-19 00:59:57 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const navBtn = document.getElementById("navBtn");
+const navMenu = document.getElementById("navMenu");
+
+navBtn.addEventListener("click", function () {
+  navMenu.classList.toggle("active");
+});
