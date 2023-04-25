aa03980f1c3f796669300927b36a20ac90aae42c
kengo1226 <kengo1226size@gmail.com>
2023-04-25 23:37:46 +0900

    修正

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ navBtn.addEventListener("click", function () {
   navMenu.classList.toggle("active");
   navBtn.classList.toggle("active");
 });
+
+// swiperr
+if (window.innerWidth < 768) {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1.2,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
